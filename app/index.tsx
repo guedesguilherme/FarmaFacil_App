2e2221bdeff864a9e1333e8af43c8c99edc58045
guilherme-guedes130105 <gryan.guedes@gmail.com>
2025-04-06 10:46:46 -0300

    chore: cria projeto expo + tailwind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+
+export default function Index() {
+  return (
+    <View>
+      <Text className='text-blue-600'>
+      </Text>
+    </View>
+  );
+}
